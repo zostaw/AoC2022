eb11db0aa9a73a1efe9e5c373d647668e3cdab89
Mateusz Kowalkowski <mateusz.kowalkowski@gmail.com>
2023-01-07 10:47:37 +0100

    BEAUTIFUL puzzle <3

--- day2_b.py
+++ day2_b.py
@@ -8,43 +8,43 @@ def read_file(file_name="day2.in"):
     return column_a, column_b
 
 
-def calculate_round(opponent_move, my_expected_result):
+def calculate_round(opponent_move, expected_outcome):
 
     if opponent_move not in ("A", "B", "C"):
         raise ValueError(
             f"opponent_move is not legal. It is {opponent_move}, while it should be one of A, B, C (str)."
         )
-    if my_expected_result not in ("X", "Y", "Z"):
+    if expected_outcome not in ("X", "Y", "Z"):
         raise ValueError(
-            f"my_expected_result is not legal. It is {my_expected_result}, while it should be one of X, Y, Z (str)."
+            f"expected_outcome is not legal. It is {expected_outcome}, while it should be one of X, Y, Z (str)."
         )
 
     # matrix of possible results,
-    # row - result dependent on expected result
+    # row - result dependent on expected outcome
     # column - result dependent on opponent's move
     results_matrix = [[3, 1, 2], [4, 5, 6], [8, 9, 7]]
 
     # transform ascii into matrix id
     # X -> id 0
-    my_expected_result = ord(my_expected_result) - 88
+    expected_outcome = ord(expected_outcome) - 88
     # A -> id 0
     opponent_move = ord(opponent_move) - 65
 
-    result = results_matrix[my_expected_result][opponent_move]
+    result = results_matrix[expected_outcome][opponent_move]
 
     return result
 
 
-def calculate_score(opponent_moves, my_expected_results):
+def calculate_score(opponent_moves, expected_outcomes):
     my_score = 0
-    for opponent_move, my_expected_result in zip(opponent_moves, my_expected_results):
-        my_score += calculate_round(opponent_move, my_expected_result)
+    for opponent_move, expected_outcome in zip(opponent_moves, expected_outcomes):
+        my_score += calculate_round(opponent_move, expected_outcome)
 
     return my_score
 
 
 if __name__ == "__main__":
-    opponent_moves, my_expected_results = read_file("day2.in")
-    result = calculate_score(opponent_moves, my_expected_results)
+    opponent_moves, expected_outcomes = read_file("day2.in")
+    result = calculate_score(opponent_moves, expected_outcomes)
 
     print(f"My result: {result}.")
